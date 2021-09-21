4bff4fdf8b0e2e0f0f789023c6b84d50f2902a24
Brittany Fenney <brittanyfenney@gmail.com>
2021-09-21 11:14:50 -0700

    update db location

--- server/db/db.js
+++ server/db/db.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize')
 
-const db = new Sequelize(process.env.DATABASE_RUL || 'postgres://localhost:5432/shopify-backend', {
+const db = new Sequelize(process.env.DATABASE_URL || 'postgres://localhost:5432/shopify-backend', {
   logging: false
 })
 
